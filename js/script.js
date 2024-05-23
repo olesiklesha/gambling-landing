00214b1ed10f071624ec711a07f8059ed12ad4df
olesiklesha <olesik736@gmail.com>
2024-05-23 15:08:51 +0300

    feat: add text change on btn click

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 (() => {
   const burgerBTN = document.querySelector(".burger");
+  const localeBTN = document.querySelector(".btns__item_local span");
 
   burgerBTN.addEventListener("click", () => {
     burgerBTN.classList.toggle("burger_opened");
     document.body.classList.toggle("stop-scroll");
+  });
+
+  localeBTN.addEventListener("click", () => {
+    localeBTN.textContent = localeBTN.textContent === "EN" ? "RU" : "EN";
   });
 })();
